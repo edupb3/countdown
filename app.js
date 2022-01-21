23557c254ee8845d7fd79523e0e00d3afd9a7f2d
Eduardo Batista <eduardopb3@gmail.com>
2022-01-21 13:40:55 -0300

    add days on form html

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const labelSeconds = document.querySelector("#seconds");
+const labelDays = document.querySelector("#days");
 
 
 const date = new Date("01-January-2023");
 const difference = date - new Date();
-let seconds = (difference / 1000).toFixed(2)
+let seconds = (difference / 1000).toFixed(2);
 let minutes = (seconds / 60).toFixed(2);
 let hours = (minutes / 60).toFixed(2);
-let days = (hours / 24).toFixed(2);
+let days = Number.parseInt(hours / 24);
 console.log(difference, seconds, minutes, hours, days);
+
+labelDays.textContent = days;
