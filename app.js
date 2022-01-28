37c903fd69f0ab4c4d8eeb2166cb2d95e3265d74
Eduardo Batista <eduardopb3@gmail.com>
2022-01-28 14:41:43 -0300

    update get year automatic

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const labelSeconds = document.querySelector("#seconds");
 const labelMinutes = document.querySelector("#minutes");
 const labelHours = document.querySelector("#hours");
 const labelDays = document.querySelector("#days");
+const labelYear = document.querySelector("#year");
 
-const targetDate = new Date("01-January-2023");
-console.log(new Date().getFullYear()+1);
+const targetDay = "01";
+const targetMonth = "01";
+const targetYear = new Date().getFullYear()+1;
+const targetDate = new Date(targetDay+"-"+targetMonth+"-"+targetYear);
 
 setInterval(() => {
     const current = new Date();
@@ -14,10 +17,20 @@ setInterval(() => {
     let minutes = Math.floor(difference / 1000 / 60 ) % 60;
     let seconds = Math.floor(difference / 1000) % 60;
 
+    hours   = hours < 10   ? "0" + hours   : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
     labelDays.textContent = days;
     labelHours.textContent = hours;
     labelMinutes.textContent = minutes;
     labelSeconds.textContent = seconds;
+    labelYear.textContent = targetYear;
     
 }, 1000);
 
+/**
+ * fazer o ano mudar automaticamente
+ * fazer o spinner funcionar 
+ * refatorar o código
+ * */
